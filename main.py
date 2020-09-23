353ff687e0f5fc2cdca8e0fac5fb3f4d498729ba
sanderau <austin.n.sanders@gmail.com>
2020-09-22 18:45:49 -0700

    my first endpoint

---
+++ main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3.8
+
+import flask
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+
+@app.route('/', methods=['GET'])
+def home():
+	return "<h1> Hello World! </h1>"
+
+app.run()
