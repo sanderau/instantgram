12e70f0e3af546376d5650e9cd40c6a20f37dfd3
sanderau <austin.n.sanders@gmail.com>
2020-09-26 14:13:13 -0700

    adding some requirments

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3.8
 
 import flask
+import sqlalchemy
+import pymysql
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
@@ -8,5 +10,19 @@ app.config["DEBUG"] = True
 @app.route('/', methods=['GET'])
 def home():
 	return "<h1> Hello World! </h1>"
+
+@app.route('/login', methods=['GET'])
+def login(req, res):
+	username = req.username
+	password = req.password
+
+	#hash the password
+
+	#ask mysql if this combo exists
+
+	#return token if correct
+	#false if not
+
+	return res
 
 app.run()
